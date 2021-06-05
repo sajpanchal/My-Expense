e63b2592591181865e22a5fec30fb4c87a10da8b
Saj Panchal <sajpanchal@gmail.com>
2021-06-05 19:29:37 -0400

    linked daily view to add expenses screen. added a button to add more entries in daily view table.

--- PersonalAccountingApp/HistoryViewController.swift
+++ PersonalAccountingApp/HistoryViewController.swift
@@ -10,11 +10,14 @@ import CoreData
 class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let year = Calendar.current.component(.year, from: Date())
     var entries: [Expense]?
+
     @IBOutlet weak var monthLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var monthStepper: UIStepper!
     @IBOutlet weak var yearStepper: UIStepper!
     @IBOutlet weak var historyTableView: UITableView!
+    @IBOutlet weak var monthTotalLabel: UILabel!
+    var monthTotal: Double = 0.0
     var leapYear: Bool {
         let year = Int(yearStepper.value)
         if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
@@ -26,11 +29,23 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        historyTableView.delegate = self
-        historyTableView.dataSource = self
-        historyTableView.rowHeight = 90        // Do any additional setup after loading the view.
-        yearStepper.value = Double(year)
        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+       
+        
+        DispatchQueue.main.async {
+            print(self.entries!)
+            self.historyTableView.delegate = self
+            self.historyTableView.dataSource = self
+            self.historyTableView.rowHeight = 90
+            self.monthStepper.value = Double(Calendar.current.component(.month, from: Date()))// Do any additional setup after loading the view.
+            self.yearStepper.value = Double(self.year)
+            self.setMonthLabel()
+            self.CalculateMonthTotal()
+            self.historyTableView.reloadData()
+            self.monthTotalLabel.text = "Month Total: $" + String(format: "%.2f", self.monthTotal)
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let dailyExpenses = segue.destination as! DailyExpensesViewController
@@ -58,6 +73,15 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     /* method to update month on stepper value change */
     @IBAction func monthStepperChanged(_ sender: Any) {
+        setMonthLabel()
+        DispatchQueue.main.async {
+            self.historyTableView.reloadData()
+            self.CalculateMonthTotal()
+            self.monthTotalLabel.text = "Month Total: $" + String(format: "%.2f", self.monthTotal)
+        }
+        
+    }
+    func setMonthLabel() {
         switch monthStepper.value {
             case 1:
                 monthLabel.text = "Jan"
@@ -86,12 +110,15 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
             default:
                 monthLabel.text = "not found"
         }
-        historyTableView.reloadData()
     }
     /* method to update the year on stepper value change.*/
     @IBAction func yearStepperChanged(_ sender: Any) {
         yearLabel.text = String(Int(yearStepper.value))
-        historyTableView.reloadData()
+        DispatchQueue.main.async {
+            self.historyTableView.reloadData()
+            self.CalculateMonthTotal()
+            self.monthTotalLabel.text = "Month Total: $" + String(format: "%.2f", self.monthTotal)
+        }
     }
     /* using tableview delegate method to return the number of rows of table based on the month selected.*/
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -115,20 +142,39 @@ class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
-    /* using tableview delegate method to create a cell object and returning it.*/
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // this will link the custom cell (HistoryTableViewCell) on each indexPath of our tableview and creates a cell object.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "history", for: indexPath) as! HistoryTableViewCell
-        cell.dayLabel.text = "\(indexPath.row + 1)"
-        cell.amountLabel.text = "$0.0"
+    
+    func CalculateMonthTotal() {
         if let items = entries {
+            monthTotal = 0.0
             for item in items {
-                if Calendar.current.component(.day, from: item.date!) == (indexPath.row + 1) && Calendar.current.component(.month, from: item.date!) == Int(monthStepper.value) && Calendar.current.component(.year, from: item.date!) == Int(yearStepper.value){
-                    cell.dayLabel.text = "\(indexPath.row + 1)"
-                    cell.amountLabel.text = "$\(item.totalAmount)"
+                if (Calendar.current.component(.month, from: item.date!) == Int(monthStepper.value)) && (Calendar.current.component(.year, from: item.date!) == Int(yearStepper.value)){
+                    monthTotal += item.totalAmount
+                    print(monthTotal)
                 }
             }
         }
+       
+    }
+    /* using tableview delegate method to create a cell object and returning it.*/
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // this will link the custom cell (HistoryTableViewCell) on each indexPath of our tableview and creates a cell object.
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "history", for: indexPath) as! HistoryTableViewCell
+        cell.dayLabel.text = String(format: "%02d",(indexPath.row + 1))
+        cell.amountLabel.text = ""
+        if let items = entries {
+         
+            for item in items {
+                if Calendar.current.component(.day, from: item.date!) == (indexPath.row + 1) && Calendar.current.component(.month, from: item.date!) == Int(monthStepper.value) && Calendar.current.component(.year, from: item.date!) == Int(yearStepper.value){
+                    cell.dayLabel.text = String(format: "%02d",(indexPath.row + 1))
+                    cell.amountLabel.text = "$"+String(format:"%.2f",(item.totalAmount))
+                  
+                 
+                    
+                }
+            }
+        }
+       // monthTotal = 0.0
         return cell
     }
     
