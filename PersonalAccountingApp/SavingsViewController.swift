d3fdf5bac7d2cc41a8b351939da081c56a0fa507
Saj Panchal <sajpanchal@gmail.com>
2021-06-20 20:34:51 -0400

    savings vc and table view cell outlets and actions connected to file. initial code setup.

--- PersonalAccountingApp/SavingsViewController.swift
+++ PersonalAccountingApp/SavingsViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class SavingsViewController: UIViewController {
-
+class SavingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     
     @IBOutlet weak var savingsTableView: UITableView!
     @IBOutlet weak var yearStepper: UIStepper!
@@ -16,14 +16,61 @@ class SavingsViewController: UIViewController {
     @IBAction func earningsButton(_ sender: Any) {
     }
     @IBAction func yearStepperChanged(_ sender: Any) {
+        yearLabel.text = String(Int(yearStepper.value))
+        self.savingsTableView.reloadData()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        yearStepper.value = Double(Calendar.current.component(.year, from: Date()))
+        savingsTableView.delegate = self
+        savingsTableView.dataSource = self
+        yearLabel.text = String(Calendar.current.component(.year, from: Date()))
         // Do any additional setup after loading the view.
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        12
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = savingsTableView.dequeueReusableCell(withIdentifier: "savings", for: indexPath) as! SavingsTableViewCell
+        cell.monthLabel.text = getMonth(number: indexPath.row) + " " + yearLabel.text!
+        return cell
+    }
+    func getMonth(number: Int) -> String {
+        switch number {
+            case 0:
+                return "Jan"
+            case 1:
+                return "Feb"
+            case 2:
+                return "Mar"
+            case 3:
+                return "Apr"
+            case 4:
+                return "May"
+            case 5:
+                return "Jun"
+            case 6:
+                return "Jul"
+            case 7:
+                return "Aug"
+            case 8:
+                return "Sep"
+            case 9:
+                return "Oct"
+            case 10:
+                return "Nov"
+            case 11:
+                return "Dec"
+            default:
+                return ""
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
