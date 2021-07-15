601b5d37127990c746d7f0b996f5ab3d6a42fc22
Saj Panchal <sajpanchal@gmail.com>
2021-07-15 14:36:11 -0400

    loginVC added with SignInWithAppleID Button.

---
+++ PersonalAccountingApp/SignInWithAppleViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SignInWithAppleViewController.swift
+//  PersonalAccountingApp
+//
+//  Created by saj panchal on 2021-07-15.
+//
+
+import UIKit
+import AuthenticationServices
+
+class SignInWithAppleViewController: UIViewController, ASAuthorizationControllerDelegate {
+
+    @IBAction func signInWithAppleBtn(_ sender: Any) {
+        print("button clicked")
+        
+    }
+
+    @IBOutlet weak var buttonView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+signInWIthAppleButton()
+        // Do any additional setup after loading the view.
+    }
+    
+    func signInWIthAppleButton() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.addTarget(self, action: #selector(handleAppleIdRequest), for: .touchUpInside)
+        authorizationButton.cornerRadius = 1
+        
+        self.buttonView.addSubview(authorizationButton)
+    }
+    @objc func handleAppleIdRequest() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+    /* this function is called after icloud account is signed in.*/
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            if !userIdentifier.isEmpty {
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let tabBarController = storyboard.instantiateViewController(identifier: "TabBarController")
+                (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(tabBarController)
+            }
+            print("UserId:\(userIdentifier)")
+            print("FullName:\(fullName)")
+            print("email:\(email)")
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
