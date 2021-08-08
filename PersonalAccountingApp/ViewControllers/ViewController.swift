e06cbb633e80a98bd0e8c2589b37e51654329532
Saj Panchal <sajpanchal@gmail.com>
2021-08-07 21:31:04 -0400

    Grouped ViewControllered files in folders. Refactored ViewController code.

--- PersonalAccountingApp/ViewControllers/ViewController.swift
+++ PersonalAccountingApp/ViewControllers/ViewController.swift
@@ -9,17 +9,20 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController, UITabBarControllerDelegate {
-    let dateFormatter: DateFormatter = DateFormatter() // date formater object
-    static var showActivtyIndicator = true
     @IBOutlet weak var selectedDate: UIDatePicker!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var addExpenseBtn: UIButton!
-   
     
+    static var showActivtyIndicator = true
     var items: [Expense] = Expense.fetchRecords()
-    
- 
     var strLabel = UILabel(frame: CGRect(x: 50, y: 0, width: 250, height: 46))
+    
+    var dateFormatter: DateFormatter {
+       let formatter = DateFormatter() // date formater object
+        formatter.dateFormat = "MMM d, yyyy" //date formatter string
+        return formatter
+    }
+
     var progressView: UIProgressView = {
         let progressView = UIProgressView(progressViewStyle: .bar)
         progressView.trackTintColor = .gray
@@ -27,6 +30,37 @@ class ViewController: UIViewController, UITabBarControllerDelegate {
         return progressView
     }()
   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addExpenseBtn.layer.cornerRadius = 5
+        toggleTabBarAvailability(isEnabled: false)
+        
+        if Self.showActivtyIndicator {
+            setViewOpacity(alpha: 0.0)
+            activityIndicator("Syncing data with iCloud...")
+            
+            for x in 0..<20 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(x)), execute: {
+                    self.progressView.setProgress(Float(x)/19, animated: true)
+                   // print(x)
+                })
+            }
+            
+            self.view.isUserInteractionEnabled = false
+            
+            Timer.scheduledTimer(withTimeInterval: 20.0, repeats: false) { _ in
+                //print("Timer stopped")
+                self.progressView.removeFromSuperview()
+                self.strLabel.removeFromSuperview()
+                self.setViewOpacity(alpha: 1.0)
+                self.view.isUserInteractionEnabled = true
+                self.toggleTabBarAvailability(isEnabled: true)
+            }
+        }
+        
+        tabBarController?.delegate = self
+        self.items = Expense.fetchRecords()
+    }
     @IBAction func dateUpdated(_ sender: Any) {
         self.items = Expense.fetchRecords()
         dateFormatter.dateFormat = "MMM d, yyyy" //date formatter string
@@ -42,72 +76,24 @@ class ViewController: UIViewController, UITabBarControllerDelegate {
         strLabel.textColor = .systemBlue
         strLabel.textAlignment = .center
         strLabel.backgroundColor = .systemBackground
-     
+        
         progressView.frame = CGRect(x: view.frame.midX - 125, y: strLabel.frame.maxY, width: 250, height: 46)
         progressView.setProgress(0.0, animated: true)
-   
+        
         view.addSubview(strLabel)
         view.addSubview(progressView)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addExpenseBtn.layer.cornerRadius = 5
-        CKContainer.default().fetchUserRecordID(completionHandler: {
-            (recordID, error) in
-            if let name = recordID?.recordName {
-                print("iCloud ID: ", name)
-            }
-            else if let error = error {
-                print(error.localizedDescription)
-            }
-        })
-       // activityView.center = self.view.center
+    func toggleTabBarAvailability(isEnabled: Bool) {
         if let arrayOfTabBarItems = tabBarController?.tabBar.items {
             for item in arrayOfTabBarItems {
-                item.isEnabled = false
+                item.isEnabled = isEnabled
             }
         }
-      
-        if Self.showActivtyIndicator {
-            selectedDate.alpha = 0.0
-            titleLabel.alpha = 0.0
-            addExpenseBtn.alpha = 0.0
-            activityIndicator("Syncing data with iCloud...")
-            for x in 0..<20 {
-                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(x)), execute: {
-                    self.progressView.setProgress(Float(x)/19, animated: true)
-                    print(x)
-                })
-            }
-           
-            self.view.isUserInteractionEnabled = false
-            print("Timer started")
-            Timer.scheduledTimer(withTimeInterval: 20.0, repeats: false) { _ in
-                print("Timer stopped")
-                self.progressView.removeFromSuperview()
-                self.strLabel.removeFromSuperview()
-                
-                self.selectedDate.alpha = 1.0
-                self.titleLabel.alpha = 1.0
-                self.addExpenseBtn.alpha = 1.0
-                self.view.isUserInteractionEnabled = true
-                
-                if let arrayOfTabBarItems = self.tabBarController?.tabBar.items {
-                    for item in arrayOfTabBarItems {
-                        item.isEnabled = true
-                    }
-                }
-            }
-        }
-       
-        tabBarController?.delegate = self
-        
-        self.items = Expense.fetchRecords()
-        for item in self.items {
-            print(item.date!)
-        }
-        dateFormatter.dateFormat = "MMM d, yyyy" //date formatter string
-        
+    }
+    func setViewOpacity(alpha: CGFloat) {
+        self.selectedDate.alpha = alpha
+        self.titleLabel.alpha = alpha
+        self.addExpenseBtn.alpha = alpha
     }
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         self.items = Expense.fetchRecords()
@@ -122,11 +108,10 @@ class ViewController: UIViewController, UITabBarControllerDelegate {
         }
       
         else {
-            
+            //code
         }
     }
     
-   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // this will set the segue's destination prop as a AddExpensesVIewController
         let addExpenses = segue.destination as! AddExpensesVIewController
@@ -143,3 +128,13 @@ class ViewController: UIViewController, UITabBarControllerDelegate {
 
 }
 
+//-------------------- Commented Code --------------------/
+/*CKContainer.default().fetchUserRecordID(completionHandler: {
+ (recordID, error) in
+ if let name = recordID?.recordName {
+ print("iCloud ID: ", name)
+ }
+ else if let error = error {
+ print(error.localizedDescription)
+ }
+ })*/
